45dae548b3adadd07229c285e97bd26f9be529b2
vardhanpedapaga-gif <vardhanpedapaga@gmail.com>
2026-06-08 21:53:32 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyClKgfxNaBxpu5GH7PBe0sRTy5HKuN84Lk",
+  authDomain: "fabor-1503.firebaseapp.com",
+  projectId: "fabor-1503",
+  storageBucket: "fabor-1503.firebasestorage.app",
+  messagingSenderId: "398403185616",
+  appId: "1:398403185616:web:bcf1d7e8ddb767cfd6a7d5"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+function login() {
+  alert("LOGIN CLICKED");
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      alert("Welcome " + result.user.displayName);
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+}
